75eb961de41cc68d6c7a103e9ac8fb5fd1d4e92a
sbx_user1051 <sbx_user1051@169.254.14.201>
2024-03-02 13:14:36 +0000

    Sat Mar 02 13:14:36 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -40,7 +40,7 @@ public class CheckoutSolution {
 
         for (String s : compressedValues) {
             if (s.contains("A")) {
-                List<String> nResult = decompose(Integer.valueOf(s.substring(0, 1)), 3, 5, s.substring(1, 2));
+                List<String> nResult = decompose(getIntPrefix(s), 3, 5, getStringSuffix(s));
                 itemsToAdd.addAll(nResult);
                 itemsToRemove.add(s);
             }
@@ -181,6 +181,7 @@ public class CheckoutSolution {
         return result;
     }
 }
+
 
 
 
