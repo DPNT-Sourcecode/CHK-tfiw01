012847f95742968088c54f518c15b25dfdbc9964
sbx_user1051 <sbx_user1051@169.254.14.193>
2024-03-02 23:58:34 +0000

    Sat Mar 02 23:58:34 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -436,11 +436,12 @@ class CustomComparatorClass implements Comparator<String> {
 
     @Override
     public int compare(String s, String t1) {
-        if (dataMap.getOrDefault(s, 0) > dataMap.getOrDefault(t1, 0)) {
+        if (dataMap.get(s) > dataMap.get(t1)) {
             return -1;
         } else {
             return 0;
         }
     }
 }
+
 
