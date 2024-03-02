0a431990dd61d8c9ff06560376a93e24014eedc2
sbx_user1051 <sbx_user1051@169.254.14.201>
2024-03-02 11:27:06 +0000

    Sat Mar 02 11:27:06 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,6 +1,7 @@
 package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -30,13 +31,22 @@ public class CheckoutSolution {
         return -1;
     }
 
-    public List<String> decompose(Integer count, Integer mod) {
-        while (mod % count != 0) {
-            
+    public List<Integer> decompose(Integer count, Integer mod) {
+        List<Integer> list = new ArrayList<>();
+        while (true) {
+            int x = mod % count;
+            if (x == 0) {
+                break;
+            }
+            list.add(x);
             mod -= 3;
         }
 
+        int lastValue = count - 
+
+        list.add()
     }
 }
+
 
 
